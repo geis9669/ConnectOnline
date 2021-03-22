558058c3cdecbade58e308108e40602d2456374d
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-03-22 02:06:55 -0600

    added main method to use sort through information I have gotten

--- src/botw/test.java
+++ src/botw/test.java
@@ -1,7 +1,52 @@
 package botw;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class test {
+	public static void main(String[] args) {
+		
+		try {
+			Set<KorokSeed> korokSeedsM = new HashSet<>();
+			String[] webSites = {"Akkala", "Central_Hyrule", "Dueling_Peaks", 
+								 "Eldin", "Faron", "Gerudo_Wasteland", 
+								 "Gerudo_Highland", "Woodland", "Great_Plateau", 
+								 "Hateno", "Hebra", "Lake",
+								 "Lanayru", "Ridgeland", "Tabantha"};
+			
+			for(String s : webSites)
+			{
+				// create the URL class object
+				URL url = new URL("https://www.ign.com/wikis/the-legend-of-zelda-breath-of-the-wild/"+s+"_Korok_Seeds");
+				Set<KorokSeed> tempKorokSeeds = getKorokSeedSet(url);
+				for(KorokSeed seed : tempKorokSeeds) {
+//					if(seed.containsString("_Korok_Seed_"))/*"id=\\"+s+*/ {
+						korokSeedsM.add(seed);
+//					}
+				}
+			}
+			
+			System.out.println(korokSeedsM.size());
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("url");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+//		System.out.println("number of items: " + korokSeeds.size());
+		// print the info I want to know.
+	}
+
 	private static Set<KorokSeed> getKorokSeedSet(URL url) throws IOException {
 		// retrieve the contents from the url
 		Scanner sc = new Scanner(url.openStream());
