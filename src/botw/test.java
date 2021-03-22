0be340c765b04fc4e27d2e48e1b2ce9115cf2985
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-03-22 02:50:45 -0600

    changed KorokSeed to String

--- src/botw/test.java
+++ src/botw/test.java
@@ -13,27 +13,67 @@ public class test {
 	public static void main(String[] args) {
 		
 		try {
-			Set<KorokSeed> korokSeedsM = new HashSet<>();
+			Set<String> korokSeedsM = new HashSet<>();
 			String[] webSites = {"Akkala", "Central_Hyrule", "Dueling_Peaks", 
 								 "Eldin", "Faron", "Gerudo_Wasteland", 
 								 "Gerudo_Highland", "Woodland", "Great_Plateau", 
-								 "Hateno", "Hebra", "Lake",
-								 "Lanayru", "Ridgeland", "Tabantha"};
+								  "Hebra", "Lake",
+								 "Lanayru", "Ridgeland", "Tabantha"};//Hateno
 			
 			for(String s : webSites)
 			{
 				// create the URL class object
 				URL url = new URL("https://www.ign.com/wikis/the-legend-of-zelda-breath-of-the-wild/"+s+"_Korok_Seeds");
-				Set<KorokSeed> tempKorokSeeds = getKorokSeedSet(url);
-				for(KorokSeed seed : tempKorokSeeds) {
-//					if(seed.containsString("_Korok_Seed_"))/*"id=\\"+s+*/ {
+				String separator = "\\n\\u003c/p\\u003e\",\"tag\":\"p\",\"header\":false,\"block\":true}},{\"__typename\":\"WikiPageElement\"";// "\nid=\\"; //"\n-\n"
+
+				Set<String> tempKorokSeeds = getKorokSeedSet(url, separator);
+				
+				int howmanyadded = 0;
+				for(String seed : tempKorokSeeds) {
+					if(seed.contains("id=\\\""+s+"_Korok_Seed_"))/* id=\"Akkala_Korok_Seed_ */ {
 						korokSeedsM.add(seed);
-//					}
+						howmanyadded ++;
+					}
+				}
+				
+				System.out.println(tempKorokSeeds.size()+" added: "+howmanyadded);
+			}
+			
+			//need to process Hateno separately Because it saves differently. also got some not right answers
+			URL url = new URL("https://www.ign.com/wikis/the-legend-of-zelda-breath-of-the-wild/Hateno_Korok_Seeds");
+			String separator = ".";
+			Set<String> tempKorokSeeds = getKorokSeedSet(url, separator);
+			int howmanyadded = 0;
+			for(String seed : tempKorokSeeds) {
+				if(seed.contains("Korok")){
+					korokSeedsM.add(seed);
+					howmanyadded ++;
 				}
 			}
+			System.out.println(tempKorokSeeds.size()+" added: "+howmanyadded);
 			
 			System.out.println(korokSeedsM.size());
 
+			String[] keyWords = {"arrow", "ballon"};
+			
+			String toSave = "";
+			for(String item : korokSeedsM){
+				
+				boolean additem = false;
+				int i = 0;
+				while(!additem && i < keyWords.length) {
+					if(item.contains(keyWords[i])) {
+						toSave += "\n:::\n"+ item+"\n:::\n";
+						//have it save only the id. so I will need to figure something else out for Hateno
+						additem = true;
+					}
+					i++;
+				}
+			}
+			
+			saveFile(toSave,"arrow_ballon");
+			
+			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -47,7 +87,7 @@ public class test {
 		// print the info I want to know.
 	}
 
-	private static Set<KorokSeed> getKorokSeedSet(URL url) throws IOException {
+	private static Set<String> getKorokSeedSet(URL url,  String separator) throws IOException {
 		// retrieve the contents from the url
 		Scanner sc = new Scanner(url.openStream());
 		// Instantiate StringBuffer to hold the results
@@ -61,24 +101,23 @@ public class test {
 		result = result.replaceAll("<[^>]*>", "");// <[^>]*>
 //		System.out.println("Contents of the web page: " + result);
 
-		HashSet<KorokSeed> korokSeeds = new HashSet<>();
+		HashSet<String> korokSeeds = new HashSet<>();
 		int nexti;
-		String separator = "\\n\\u003c/p\\u003e\",\"tag\":\"p\",\"header\":false,\"block\":true}},{\"__typename\":\"WikiPageElement\"";// "\nid=\\"; //"\n-\n"
-
+		
 		for (int i = result.indexOf(separator); i > 0; i = nexti) {
 			nexti = result.indexOf(separator, i + 1);
 			String info;
 			if (nexti >= 0) {
 				info = result.substring(i, nexti);
-				korokSeeds.add(new KorokSeed(info));
+				korokSeeds.add((info));
 			}
 		}
 		return korokSeeds;
 
 	}
 
-	private static void saveFile(String info) {
-		String filename = "KorokSeeds.txt";
+	private static void saveFile(String info, String name) {
+		String filename = "KorokSeeds"+name+".txt";
 
 		try (FileOutputStream saveStream = new FileOutputStream(filename);
 				ObjectOutputStream output = new ObjectOutputStream(saveStream)) {
