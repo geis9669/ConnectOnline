33c9d71fe3901807f1dd8b5fdd32f646a8069268
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-03-22 02:04:24 -0600

    created to hold data about a korok seed
    implmented equals and hashcode

---
+++ src/botw/KorokSeed.java
@@ -0,0 +1,33 @@
+package botw;
+
+public class KorokSeed {
+	private String info;
+	
+	public KorokSeed(String info){
+		this.info = info;
+	}
+	
+	public boolean containsString(String str) {
+		int index = info.indexOf(str);
+		return index > -1;
+//		return info.contains(str);
+	}
+	
+	public boolean equals(Object other){
+		
+		if(this == other){
+			return true;
+		}
+		
+		if(!(other instanceof KorokSeed)){
+			return false;
+		}
+		KorokSeed o = (KorokSeed) other;
+		return info.equals(o.info);
+	}
+	
+	public int hashCode(){
+		return info.hashCode();
+	}
+
+}
