d04c2247b7becb53a8bbd10172cd6a29e97d204a
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-03-22 02:06:11 -0600

    refactored out method to be able to use it multiple times

--- src/botw/test.java
+++ src/botw/test.java
@@ -2,6 +2,35 @@ package botw;
 
 
 public class test {
+	private static Set<KorokSeed> getKorokSeedSet(URL url) throws IOException {
+		// retrieve the contents from the url
+		Scanner sc = new Scanner(url.openStream());
+		// Instantiate StringBuffer to hold the results
+		StringBuffer sb = new StringBuffer();
+		while (sc.hasNext()) {
+			sb.append(sc.next() + "\n");
+		}
+
+		String result = sb.toString();
+//		System.out.println(result);
+		result = result.replaceAll("<[^>]*>", "");// <[^>]*>
+//		System.out.println("Contents of the web page: " + result);
+
+		HashSet<KorokSeed> korokSeeds = new HashSet<>();
+		int nexti;
+		String separator = "\\n\\u003c/p\\u003e\",\"tag\":\"p\",\"header\":false,\"block\":true}},{\"__typename\":\"WikiPageElement\"";// "\nid=\\"; //"\n-\n"
+
+		for (int i = result.indexOf(separator); i > 0; i = nexti) {
+			nexti = result.indexOf(separator, i + 1);
+			String info;
+			if (nexti >= 0) {
+				info = result.substring(i, nexti);
+				korokSeeds.add(new KorokSeed(info));
+			}
+		}
+		return korokSeeds;
+
+	}
 
 	private static void saveFile(String info) {
 		String filename = "KorokSeeds.txt";
