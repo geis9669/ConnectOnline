d8d29703e1bb3cf582be31643b6e1573860d8723
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-04-08 02:25:10 -0600

    started reading in and parsing the file not sure how exactly to parse
    I am figuring out what are the starts and ends that I need to look out for so when I change and then save the file it will acutally keep the chages

--- src/botw/BotwController.java
+++ src/botw/BotwController.java
@@ -1,5 +1,35 @@
 package botw;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class BotwController {
+	
+	public void start()
+	{
+		String inputHtml = "C:/Users/grego/Desktop/hyruleMapCopy.html";
+		File inputFile = new File(inputHtml);
+		
+		List<String> htmlfile = new ArrayList<>();
+		Stack<String> lastToken = new Stack<>();
+		
+		try(Scanner reader = new Scanner(inputFile);) {
+			while (reader.hasNext("<")) {
+				String temp = reader.next(">");
+				
+				
+				
+				htmlfile.add(temp);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
