6bbcc733aa1e0308c010dc342dfbaef5f1edfec1
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-05-28 02:30:07 -0600

    added method to get the desired data from a html file.

--- src/botw/BotwController.java
+++ src/botw/BotwController.java
@@ -2,34 +2,87 @@ package botw;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class BotwController {
+	private String inputHtml = "C:/Users/grego/Desktop/hyruleMapCopy.html";
+	private Document htmlDoc;
+	
+	
+	public BotwController() throws IOException
+	{
+//		this.inputHtml = htmlDocPathName;
+		File inputFile = new File(inputHtml);
+		this.htmlDoc = Jsoup.parse(inputFile, "UTF-8", "https://www.ign.com/maps/the-legend-of-zelda-breath-of-the-wild/hyrule");
+
+	}
 	
 	public void start()
 	{
 		String inputHtml = "C:/Users/grego/Desktop/hyruleMapCopy.html";
 		File inputFile = new File(inputHtml);
 		
-		List<String> htmlfile = new ArrayList<>();
-		Stack<String> lastToken = new Stack<>();
-		
-		try(Scanner reader = new Scanner(inputFile);) {
-			while (reader.hasNext("<")) {
-				String temp = reader.next(">");
+		try (Scanner reader = new Scanner(inputFile);){
+			Document doc = Jsoup.parse(inputFile, "UTF-8", "https://www.ign.com/maps/the-legend-of-zelda-breath-of-the-wild/hyrule");
+			Elements content = doc.children().get(0).children().get(1).children().get(3).children().get(0).children().get(1).children().get(0).children().get(0).children().get(0).children().get(0).children();// doc.getAllElements();
+			
+			Element c = content.get(3);
+			String list = c.html();
+			String[] listItems = list.split("<div");
+			
+			
+			// go through the list and remove what I don't want
+			
+			// go through the original document to where the list starts, 
+			// then add what is in the listItems
+			// then add the last part of the text.
+			
+			while (reader.hasNext()) {
+				reader.next();
 				
-				
-				
-				htmlfile.add(temp);
 			}
-		} catch (FileNotFoundException e) {
+			
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		
 	}
+	
+	//List<String> other thing to return.
+	public String[] gettingMapMarkers()
+	{
+		Elements content = htmlDoc.children().get(0).children().get(1).children().get(3).children().get(0).children().get(1).children().get(0).children().get(0).children().get(0).children().get(0).children();// doc.getAllElements();
+		Element c = content.get(3);
+		String spilter = ("<div");
+		String list = c.html();
+		list = list.substring(spilter.length());
+		String[] listItems = list.split(spilter);
+		for(int i = 0; i< listItems.length; i++) {
+			listItems[i] = spilter + listItems[i];
+		}
+		return listItems;//Arrays.asList(listItems);
+	}
+	
+	/*
+	 * Ask user for the file to open and the webpage
+	 * read in the web page
+	 * 
+	 * find the list 
+	 * separate it out
+	 * allow the user to remove the items from the list
+	 * 
+	 * ask for the file name to save
+	 * add back the list to the original document.
+	 */
 
 }
