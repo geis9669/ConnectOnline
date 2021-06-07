dd9772e3442f2288f1020c31cbdbb0c98e1e4c69
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-06-07 14:32:25 -0600

    added a menu bar with a save button

--- src/botw/BotwFrame.java
+++ src/botw/BotwFrame.java
@@ -1,13 +1,18 @@
 package botw;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 public class BotwFrame extends JFrame{
 	private Dimension currentSize;
@@ -34,6 +39,22 @@ public class BotwFrame extends JFrame{
         	}
         });
 		
+		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu saveMenu = new JMenu("Save as");
+		saveMenu.setMnemonic(KeyEvent.VK_S);
+		saveMenu.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				/*
+				 * need to get the data and name
+				 */
+				
+			}
+		});
+		menuBar.add(saveMenu);
+		this.setJMenuBar(menuBar);
+		
 		currentSize = this.getContentPane().getSize();
 		panel.newSize(currentSize);
 		this.setVisible(true);
@@ -50,5 +71,5 @@ public class BotwFrame extends JFrame{
 			panel.newSize(newSize);
 		}
 	}
-
+	
 }
